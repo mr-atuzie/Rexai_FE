9799a634dac1d18d50e1b5ba613d2a5f94d5e48c
rex atuzie <rexatuzie@gmail.com>
2024-11-28 02:14:48 +0100

    added gemini api

--- src/components/NewPrompt.jsx
+++ src/components/NewPrompt.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import { IoIosSend } from "react-icons/io";
 import Upload from "./Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
   const [img, setImg] = useState({
@@ -9,11 +11,34 @@ const NewPrompt = () => {
     error: "",
     dbData: {},
   });
+
+  const [question, setQuesstion] = useState("");
+  const [answer, setAnswer] = useState("");
+
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behaviour: "smooth" });
   }, []);
+
+  const askAI = async (text) => {
+    setQuesstion(text);
+
+    const result = await model.generateContent(text);
+    console.log(result.response.text());
+
+    setAnswer(result.response.text());
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+
+    if (!text) return;
+
+    askAI(text);
+  };
   return (
     <>
       {img.isLoading && <div>Loading...</div>}
@@ -24,8 +49,27 @@ const NewPrompt = () => {
           width="380"
         />
       )}
+      {question && (
+        <div className=" p-4 bg-[#2c2937] rounded-xl max-w-[80%] self-end">
+          {question}
+        </div>
+      )}
+      {answer && (
+        <div>
+          <Markdown> {answer}</Markdown>
+        </div>
+      )}
+      {/* <button
+        onClick={askAI}
+        className=" bg-white w-full p-2.5 text-center rounded-full text-black"
+      >
+        TEST
+      </button> */}
       <div ref={endRef} className=" pb-[100px]" />
-      <form className=" w-[50%] bg-[#2c2937] rounded-xl flex items-center absolute bottom-0 px-4">
+      <form
+        onSubmit={handleSubmit}
+        className=" w-[50%] bg-[#2c2937] rounded-xl flex items-center absolute bottom-0 px-4"
+      >
         <Upload setImg={setImg} />
         <input className=" hidden" id="file" type="file" multiple={false} />
 
@@ -33,6 +77,7 @@ const NewPrompt = () => {
           className="bg-transparent border-none p-4 flex-1 outline-none text-[#ececec]"
           type="text"
           placeholder="Ask me anything...."
+          name="text"
         />
 
         <button className=" bg-[#605e68] border-none flex justify-center items-center p-2 h-9 w-9 rounded-full ">
